d214d964807e30a4c51e645912e0c73d32d54a92
martinss27 <leoalmeidamartins@gmail.com>
2025-03-06 17:51:46 -0300

    first commit

---
+++ text_statistics.py
@@ -0,0 +1,34 @@
+import string
+from collections import Counter
+
+
+def text_analyzer(text):
+    text = text.translate(str.maketrans('','', string.punctuation)).lower()
+
+    words = text.split()
+
+    num_words = len(words)
+    
+    num_characters = sum(len(word) for word in words)
+    
+    longest_word = max(words,key=len)
+    
+    shortest_word = min(words,key=len)
+
+    unique_words = set(words)
+
+    word_frequencies = Counter(words)
+    word_frequencies = dict(sorted(word_frequencies.items(), key=lambda item: item[1], reverse=True))
+
+    print(f"Number of words: {num_words}")
+    print(f"Number of characters: {num_characters}")
+    print(f"Longest word: {longest_word}")
+    print(f"Shortest word: {shortest_word}")
+    print(f"Unique words: {unique_words}")
+    print("Word frequencies:")
+    for word, freq in word_frequencies.items():
+        print(f"{word}: {freq}")
+
+if __name__ == "__main__":
+    text = input("Enter a text: ")
+    text_analyzer(text)
